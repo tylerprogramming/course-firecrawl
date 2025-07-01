4c8a72abcbda5b00e82338207ea5cc3eb670afc8
tylerreed <tylerreedytlearning@gmail.com>
2025-07-01 17:18:04 -0400

    added mapping for firecrawl code

---
+++ 07_map.py
@@ -0,0 +1,15 @@
+from firecrawl import FirecrawlApp
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FirecrawlApp(api_key=os.getenv("FIRECRAWL_API_KEY"))
+
+# Map a website:
+map_result = app.map_url(
+    url='https://docs.crewai.com/en',
+    include_subdomains=True,
+)
+print(map_result)
+print("Found", len(map_result.links), "pages")
